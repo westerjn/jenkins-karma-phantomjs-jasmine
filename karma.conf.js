dd2bcfcedb6784ecc7421c53b264b12bc23aeddb
Jim Sloan <jsloan@mchs.com>
2016-03-21 16:49:34 -0400

    add another plugin

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    plugins: ['karma-jasmine', 'karma-phantomjs-launcher'],
+    plugins: ['karma-jasmine', 'karma-phantomjs-launcher', 'karam-reporter-junit'],
 
     // list of files / patterns to load in the browser
     files: ['*spec.js'],
